fa1d1eeee86e49cd8252d0d77cebb3ae8ad79b66
David <tien.david.duc.pham1@gmai.com>
2024-07-28 14:08:46 +0200

    Solution for task 2.

---
+++ day1/task2.py
@@ -0,0 +1,8 @@
+user_name = input("What is your name? ")
+age = input("How old are you? ")
+colour = input("What is your favourite colour? ")
+
+if user_name == "David" and age == "15" and colour == "green":
+    print("My favourite game is Minecraft")
+else:
+    print("I don't know")
