6d074e58bb65ad8204d76ad6379a1fc728c06e7a
David <tien.david.duc.pham1@gmai.com>
2024-07-28 14:22:04 +0200

    Solution for task 2.

---
+++ day2/task2.py
@@ -0,0 +1,12 @@
+password = input("What is the password")
+
+if password == "123" or password == "David":
+    print("Successful")
+else:
+    print("error")
+    pin = input("What is the pin")
+    if pin == "12345":
+        print("Successful")
+    else:
+        print("error")
+
