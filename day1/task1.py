d3cdaa798087c4c8bfff1cfefcfb7889eaec17d3
David <tien.david.duc.pham1@gmai.com>
2024-07-28 13:50:20 +0200

    Solution for task 1.

---
+++ day1/task1.py
@@ -0,0 +1,7 @@
+user_name = input("What is your name?")
+age = input("How old are you?")
+
+if user_name == "David" and age == "15":
+    print("My favourite game is Minecraft")
+else:
+    print("I don't know")
