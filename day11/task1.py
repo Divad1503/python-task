a75d0e8ee07e7cda38c2919128e0f15f2086babf
David <tien.david.duc.pham1@gmai.com>
2024-08-09 16:32:29 +0200

    task 1 day 10

---
+++ day11/task1.py
@@ -0,0 +1,14 @@
+jahr = int(input("Welches Jahr ist es? "))
+schaltjahr = jahr % 4
+a = jahr % 400
+b = jahr % 100
+if b == 0:
+    if a == 0:
+        print(f"The year {jahr} is a leap year")
+    else:
+        print(f"Year {jahr} is not a leap year")
+else:
+    if schaltjahr == 0:
+        print(f"Year {jahr} is a leap year")
+    else:
+        print(f"Year {jahr} is not a leap year")
