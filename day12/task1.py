38ad879841951301768fbcad52555ad1074f5af3
David <tien.david.duc.pham1@gmai.com>
2024-08-11 12:52:18 +0200

    task1

--- day12/task1.py
+++ day12/task1.py
@@ -1,4 +1,4 @@
-numbers = [2,5623,5,52,345,546,325,6,643,667,89,36,7,345]
+numbers = [2,5623,5,52,345,546,325,6,514,641,4,624,12,54574,144,24357,235,7643,667,89,36,7,345]
 for i in range(len(numbers)):
     for h in range(len(numbers)-1):
         if numbers[h] > numbers[h+1]:
